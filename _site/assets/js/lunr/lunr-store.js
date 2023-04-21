ad0da69744ad6ab08592d9644605cad5f9e8dba0
Prajwal Mendon <prajwalmendon@gmail.com>
2023-04-21 11:31:01 -0700

    making a test posts

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Test",
+        "excerpt":"Test Title   by Prajwal Mendon  ","categories": [],
+        "tags": [],
+        "url": "/test/",
+        "teaser": null
+      }]
